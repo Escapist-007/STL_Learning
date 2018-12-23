996086ab9efb2616569db37bfc5db6c8f8e5a89b
Monir <moniruzzaman.023.bd.buet@gmail.com>
2018-12-23 17:23:43 -0500

    added all STL headers

--- STL_Learning/main.cpp
+++ STL_Learning/main.cpp
@@ -9,7 +9,22 @@
 //
 
 #include <iostream>
+
+// STL Headers
 #include <vector>
+#include <deque>
+#include <list>
+#include <set>  // set and multiset
+#include <map>  // map and multimap
+
+#include <unordered_set>   // unordered set/multiset
+#include <unordered_map>   // unordered map/multimap
+
+#include <iterator>
+#include <algorithm>
+#include <numeric>   // some numeric algorithm
+#include <functional>
+
 
 using namespace std;
 
