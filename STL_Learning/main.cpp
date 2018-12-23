9b8639d9aec7ba082027609bbe38b305e110374b
Escapist-007 <moniruzzaman.023.bd.buet@gmail.com>
2018-12-23 13:55:55 -0500

    added initial xcode project

---
+++ STL_Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  STL_Learning
+//
+//  Created by Md Moniruzzaman Monir on 12/23/18.
+//  Copyright © 2018 Md Moniruzzaman Monir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
